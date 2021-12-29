1534a68af245ad52fa24f8bf2a569365b347ccf2
juan351 <montiverolp@yahoo.com>
2021-12-28 21:07:59 -0300

    4.5 favoriteBlog test

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -24,3 +24,53 @@ describe('total likes', () => {
     expect(result).toBe(5)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithManyBlogs = [
+    {
+      'title': 'Internet paso a paso',
+      'author':'IP@P',
+      'url':'https://internetpasoapaso.com/',
+      'likes':25,
+      'id':'61cb9647af4335e8f3be5606'
+    },
+    {
+      'title':'Retroinvaders',
+      'author':'José Zanni',
+      'url':'https://retroinvaders.com',
+      'likes':34,
+      'id':'61cb9682af4335e8f3be5609'
+    },
+    {
+      'title':
+      'El blog de NitroPC',
+      'author':'NitroPC',
+      'url':'https://www.nitro-pc.es/blog/',
+      'likes':14,
+      'id':'61cb96c3af4335e8f3be560b'
+    },
+    {
+      'title':'Denken Über',
+      'author':'Mariano Amartino',
+      'url':'https://www.uberbin.net/',
+      'likes':10,
+      'id':'61cb9766af4335e8f3be560e'
+    },
+    {'title':'Blog de Vilma Nuñez',
+      'author':'Vilma Nuñez',
+      'url':'https://vilmanunez.com/blog/',
+      'likes':30,
+      'id':'61cb9781af4335e8f3be5610'}
+  ]
+  const finalBlog = {
+    'title':'Retroinvaders',
+    'author':'José Zanni',
+    'url':'https://retroinvaders.com',
+    'likes':34,
+    'id':'61cb9682af4335e8f3be5609'
+  }
+  test('when list has many blogs, finds the favorite', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    expect(result).toEqual(finalBlog)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  blogs.sort((a,b) => b.likes - a.likes)
+  return blogs[0]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
