28333a519e791b70a766c4631fcbd223a2b3d7cb
juan351 <montiverolp@yahoo.com>
2021-12-28 21:40:32 -0300

    4.6 mostBlogs test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,8 +11,24 @@ const favoriteBlog = (blogs) => {
   return blogs[0]
 }
 
+const mostBlogs = (blogs) => {
+  let counter = {}
+  let ordered = []
+
+  blogs.forEach(element => {
+    counter[element.author] = (counter[element.author] || 0) + 1
+  })
+  for(let element in counter){
+    ordered.push([element, counter[element]])
+  }
+  ordered.sort((a,b) => b[1] - a[1])
+
+  return {'author': ordered[0][0], 'blogs': ordered[0][1]}
+
+}
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
