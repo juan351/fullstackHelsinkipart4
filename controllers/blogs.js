0cda6df1d48ecb644119bf86c3c1a70cecb01b3a
juan351 <montiverolp@yahoo.com>
2021-12-30 08:15:40 -0300

    4.17-19 bloglist expansion authentication token

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,31 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
   response.json(blogs)
     
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
   
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if(!blog.likes){
     blog.likes = 0
@@ -18,7 +35,11 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
   }
 
+  blog.user = user._id
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
   response.json(savedBlog)
   
 })
